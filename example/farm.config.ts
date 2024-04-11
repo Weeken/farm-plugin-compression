cfb5c329a3b650dfa759beef9768a124fd9a94fb
高伟健 <921593174@qq.com>
2024-04-11 11:26:56 +0800

    test: workflow publish

--- example/farm.config.ts
+++ example/farm.config.ts
@@ -3,12 +3,12 @@ import vue from "@vitejs/plugin-vue";
 // import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-  // compilation: {
-  //   output: {
-  //     filename: "assets/[name].[hash].[ext]",
-  //     assetsFilename: "static/[resourceName].[ext]",
-  //   },
-  // },
+  compilation: {
+    output: {
+      filename: "assets/[name].[hash].[ext]",
+      assetsFilename: "static/[resourceName].[ext]",
+    },
+  },
   plugins: [["farm-plugin-compression", { level: "best" }]],
   vitePlugins: [vue()],
 });
