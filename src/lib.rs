625d94b5eb05814aa1879622a0a1cc8eef88cf37
高伟健 <921593174@qq.com>
2024-04-11 15:03:07 +0800

    chore: add some dependences

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(clippy::all)]
 
 use farmfe_core::{
-  config::Config,
+  config::{Config, Mode},
   context::CompilationContext,
   error::Result,
   plugin::{Plugin, PluginFinalizeResourcesHookParams},
@@ -46,42 +46,43 @@ impl Plugin for FarmPluginCompression {
     _param: &mut PluginFinalizeResourcesHookParams,
     _context: &Arc<CompilationContext>,
   ) -> Result<Option<()>> {
-    let options: Value = serde_json::from_str(&self.compression_option).unwrap_or_default();
+    if matches!(_context.config.mode, Mode::Production) {
+      let options: Value = serde_json::from_str(&self.compression_option).unwrap_or_default();
 
-    let mut compression = Compression::default();
-    if let Some(level) = options.get("level") {
-      let result = level.as_str().unwrap();
-      // println!("compression: {}", result);
-      match result {
-        "default" => compression = Compression::default(),
-        "none" => compression = Compression::none(),
-        "fast" => compression = Compression::fast(),
-        "best" => compression = Compression::best(),
-        _ => compression = Compression::default(),
+      let mut compression = Compression::default();
+      if let Some(level) = options.get("level") {
+        let result = level.as_str().unwrap();
+        // println!("compression: {}", result);
+        match result {
+          "default" => compression = Compression::default(),
+          "none" => compression = Compression::none(),
+          "fast" => compression = Compression::fast(),
+          "best" => compression = Compression::best(),
+          _ => compression = Compression::default(),
+        }
       }
-    }
-    let resource_map_clone = _param.resources_map.clone();
+      let resource_map_clone = _param.resources_map.clone();
 
-    for (name, resource) in resource_map_clone.iter() {
-      if resource.resource_type.to_ext() != ResourceType::Custom("gz".to_string()).to_ext() {
-        // println!("name: {}", name);
-        if !name.starts_with("FARM_RUNTIME_") {
-          let file_name = format!("{}.gz", name);
+      for (name, resource) in resource_map_clone.iter() {
+        if resource.resource_type.to_ext() != ResourceType::Custom("gz".to_string()).to_ext() {
+          // println!("name: {}", name);
+          if !name.starts_with("FARM_RUNTIME_") {
+            let file_name = format!("{}.gz", name);
 
-          let gz_resource = Resource {
-            name: file_name.clone(),
-            bytes: generate_compress_data(&resource.bytes, compression),
-            emitted: false,
-            resource_type: ResourceType::Custom("gz".to_string()),
-            origin: resource.origin.clone(),
-            info: None,
-          };
+            let gz_resource = Resource {
+              name: file_name.clone(),
+              bytes: generate_compress_data(&resource.bytes, compression),
+              emitted: false,
+              resource_type: ResourceType::Custom("gz".to_string()),
+              origin: resource.origin.clone(),
+              info: None,
+            };
 
-          insert_resource(_param.resources_map, file_name.clone(), gz_resource);
+            insert_resource(_param.resources_map, file_name.clone(), gz_resource);
+          }
         }
       }
     }
-
     Ok(None)
   }
 }
